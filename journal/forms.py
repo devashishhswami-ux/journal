d991ff76dd66dbe5d4f1949ebebdc2858d84a28b
Devashish <devashish@telegram-bot.com>
2025-12-12 15:57:58 +0530

    fix: Set email in clean() before save to fix PostgreSQL constraint
    - Override clean() method to set email in cleaned_data
    - Email is set to empty string before allauth processes the form
    - Prevents NULL email from reaching database save
    - Fixes signup 500 error on PostgreSQL/Neon database

--- journal/forms.py
+++ journal/forms.py
@@ -8,7 +8,7 @@ class CustomSignupForm(SignupForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        # Remove email field
+        # Remove email field from the form
         if 'email' in self.fields:
             del self.fields['email']
         
@@ -35,14 +35,10 @@ class CustomSignupForm(SignupForm):
         for field_name in self.fields:
             self.fields[field_name].help_text = ''
     
-    def save(self, request):
-        """Save the user with proper email handling for PostgreSQL"""
-        user = super().save(request)
-        
+    def clean(self):
+        """Override clean to set email to empty string for PostgreSQL compatibility"""
+        cleaned_data = super().clean()
         # PostgreSQL requires email to be NOT NULL
-        # If no email provided, set to empty string
-        if not user.email:
-            user.email = ''
-            user.save()
-        
-        return user
+        # Set to empty string since we don't collect email
+        cleaned_data['email'] = ''
+        return cleaned_data
